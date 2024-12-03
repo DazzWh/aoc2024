b86cf7835c2a8ff35e1d01f51f096044b79b4fbe
DazzWh <darrellwhittall@gmail.com>
2024-12-03 09:09:15 +0000

    Init

---
+++ dayOne/Program.cs
@@ -0,0 +1,20 @@
+﻿using MoreLinq;
+
+var listOne = new List<int>();
+var listTwo = new List<int>();
+
+File.ReadAllLines("input")
+    .Select(s => s.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+    .ForEach(s =>
+    {
+        listOne.Add(int.Parse(s[0]));
+        listTwo.Add(int.Parse(s[1]));
+    });
+
+var partOne = listOne.OrderDescending().Zip(listTwo.OrderDescending(),
+    (first, second) => Math.Abs(first - second)).Sum();
+
+var partTwo = listOne.Sum(i => i * listTwo.Count(x => x == i));
+
+Console.WriteLine($"PartOne: {partOne}");
+Console.WriteLine($"PartTwo: {partTwo}");
---
+++ dayTwo/Program.cs
@@ -0,0 +1,39 @@
+﻿var reports = File.ReadAllLines("input").Select(s => s.Split(' '));
+
+var partOne = reports.Where(isSafe).Count();
+var partTwo = reports.Where(isSafeWithDampener).Count();
+
+Console.WriteLine($"Part one: {partOne}");
+Console.WriteLine($"Part two: {partTwo}");
+
+return;
+
+bool isSafe(string[] report)
+{
+    var r = report.Select(int.Parse).ToList();
+    var isIncreasing = r[0] < r[1];
+    
+    for (var i = 0; i < r.Count - 1; i++)
+    {
+        if(r[i] == r[i+1]) return false;
+        if((isIncreasing && r[i] > r[i+1]) ||
+           (!isIncreasing && r[i] < r[i+1] )) return false;
+        if (Math.Abs(r[i] - r[i + 1]) > 3) return false;
+    }
+
+    return true;
+}  
+
+bool isSafeWithDampener(string[] report)
+{
+    if (isSafe(report)) return true;
+
+    for (var idx = 0; idx < report.Length; idx++)
+    {
+        var reportWithoutIdx = report.ToList();
+        reportWithoutIdx.RemoveAt(idx);
+        if(isSafe(reportWithoutIdx.ToArray())) return true;
+    }
+    
+    return false;
+}
