1c543e5c90c711711ba4c907126beb1fd8088006
DazzWh <darrellwhittall@gmail.com>
2024-12-06 13:21:01 +0000

    Changed project name scheme, added day 5 & 6

---
+++ Day05/Program.cs
@@ -0,0 +1,84 @@
+﻿var input = File.ReadAllLines("input");
+
+var orderings = input
+    .Where(l => l.Contains('|'))
+    .Select(l => l.Split('|'))
+    .Select(l => new[] { Convert.ToInt32(l[0]), Convert.ToInt32(l[1]) })
+    .ToArray();
+
+var updates = input
+    .Where(l => l.Contains(','))
+    .Select(l => l.Split(','))
+    .Select(n => n.Select(int.Parse).ToArray())
+    .ToArray();
+
+var partOne = updates
+    .Where(isInCorrectOrder)
+    .Sum(update => update[update.Length / 2]);
+
+var partTwo = updates
+    .Where(update => !isInCorrectOrder(update))
+    .Select(sortIntoCorrectOrder)
+    .Sum(update => update[update.Length / 2]);
+
+Console.WriteLine($"Part One: {partOne}");
+Console.WriteLine($"Part Two: {partTwo}");
+
+return;
+
+bool isInCorrectOrder(int[] update)
+{
+    for (var i = 0; i < update.Length; i++)
+    {
+        foreach (var order in orderings.Where(o => o[0] == update[i]))
+        {
+            if (Array.IndexOf(update, order[1]) != -1 && Array.IndexOf(update, order[1]) < i)
+            {
+                return false;
+            }
+        }
+
+        foreach (var order in orderings.Where(o => o[1] == update[i]))
+        {
+            if (Array.IndexOf(update, order[1]) != -1 && Array.IndexOf(update, order[1]) > i)
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+int[] sortIntoCorrectOrder(int[] update)
+{
+    var sorted = update.ToList();
+
+    while (!isInCorrectOrder(sorted.ToArray()))
+    {
+        for (var i = 0; i < sorted.Count; i++)
+        {
+            foreach (var order in orderings.Where(o => o[0] == sorted[i]))
+            {
+                if (sorted.IndexOf(order[1]) != -1 && sorted.IndexOf(order[1]) < i)
+                {
+                    var value = sorted[i];
+                    sorted.RemoveAt(i);
+                    sorted.Insert(sorted.IndexOf(order[1]), value);
+                }
+            }
+
+            foreach (var order in orderings.Where(o => o[1] == sorted[i]))
+            {
+                if (sorted.IndexOf(order[1]) != -1 && sorted.IndexOf(order[1]) > i)
+                {
+                    var value = sorted[i];
+                    sorted.RemoveAt(i);
+                    sorted.Insert(sorted.IndexOf(order[1] + 1), value);
+                }
+            }
+        }
+    }
+
+    return sorted.ToArray();
+}
---
+++ Day06/Program.cs
@@ -0,0 +1,69 @@
+﻿var input = File.ReadAllLines("input").Select(s => s.ToCharArray()).ToArray();
+var startPos = getGuardPosition(input);
+
+var partOne = getWalkedPath(input, startPos, null).WalkedPath.Sum(l => l.Count(c => c == 'X'));
+
+var partTwo = Enumerable.Range(0, input.Length).Select(y => Enumerable.Range(0, input[y].Length)
+    .Count(x => input[y][x] == '.' && getWalkedPath(input, startPos, (X: x, Y: y)).IsLoop)).Sum();
+
+Console.WriteLine($"Part one: {partOne}");
+Console.WriteLine($"Part two: {partTwo}");
+
+return;
+
+(char[][] WalkedPath, bool IsLoop) getWalkedPath(
+    char[][] input,
+    (int X, int Y) startPosition,
+    (int X, int Y)? blockPosition)
+{
+    var walkedPath = input.Select(l => l.ToArray()).ToArray();
+
+    if (blockPosition.HasValue)
+    {
+        walkedPath[blockPosition.Value.Y][blockPosition.Value.X] = '#';
+    }
+
+    (int X, int Y)[] directions =
+    [
+        (X: 0, Y: -1), (X: 1, Y: 0), (X: 0, Y: 1), (X: -1, Y: 0)
+    ];
+
+    var di = 0;
+    var guardPosition = startPosition;
+    var visited = new HashSet<(int D, int X, int Y)>();
+
+    while (positionIsInBounds(guardPosition))
+    {
+        if (visited.Contains((di, guardPosition.X, guardPosition.Y)))
+        {
+            return (walkedPath, true);
+        }
+
+        walkedPath[guardPosition.Y][guardPosition.X] = 'X';
+        visited.Add((di, guardPosition.X, guardPosition.Y));
+
+        while (positionIsInBounds((X: guardPosition.X + directions[di].X, Y: guardPosition.Y + directions[di].Y)) &&
+               walkedPath[guardPosition.Y + directions[di].Y][guardPosition.X + directions[di].X] == '#')
+        {
+            di = (di + 1) % directions.Length;
+        }
+
+        guardPosition.X += directions[di].X;
+        guardPosition.Y += directions[di].Y;
+    }
+
+    return (walkedPath, false);
+}
+
+bool positionIsInBounds((int X, int Y) p)
+{
+    return p.X >= 0 && p.X < input.Length &&
+           p.Y >= 0 && p.Y < input.Length;
+}
+
+(int X, int Y) getGuardPosition(char[][] input)
+{
+    var y = Array.FindIndex(input, l => l.Contains('^'));
+    var x = Array.IndexOf(input[y], '^');
+    return (x, y);
+}
