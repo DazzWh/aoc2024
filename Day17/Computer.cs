a8d9c45d6bc8294eecacb87cd3b8a84e1d94efc5
DazzWh <darrellwhittall@gmail.com>
2024-12-21 12:07:36 +0000

    Day17

---
+++ Day17/Computer.cs
@@ -0,0 +1,65 @@
+﻿public class Computer(long regA, long regB, long regC, long[] Program, bool sequenceEqualCheck = false)
+{
+    public long RegA = regA;
+    public long RegB = regB;
+    public long RegC = regC;
+    public List<long> Output = new();
+
+    public void RunProgram()
+    {
+        var instPtr = 0L;
+        while (instPtr < Program.Length - 1)
+        {
+            var opcode = Program[instPtr];
+            var literal = Program[instPtr + 1];
+            var combo = literal switch
+            {
+                4 => RegA,
+                5 => RegB,
+                6 => RegC,
+                _ => literal
+            };
+
+            switch (opcode)
+            {
+                case 0: // adv
+                    RegA = (long)(RegA / Math.Pow(2, combo));
+                    break;
+
+                case 1: // bxl
+                    RegB ^= literal;
+                    break;
+
+                case 2: // bst
+                    RegB = combo % 8;
+                    break;
+
+                case 3: // jnz
+                    if (RegA == 0) break;
+                    instPtr = literal;
+                    continue;
+
+                case 4: // bxc
+                    RegB ^= RegC;
+                    break;
+
+                case 5: // out
+                    Output.Add(combo % 8);
+                    if(sequenceEqualCheck && 
+                       (Output.Count > Program.Length ||
+                       !Output.SequenceEqual(Program[..Output.Count]))) return;
+                    break;
+
+                case 6: // bdv
+                    RegB = (long)(RegA / Math.Pow(2, combo));
+                    break;
+
+                case 7: // cdv
+                    RegC = (long)(RegA / Math.Pow(2, combo));
+                    break;
+            }
+
+            instPtr += 2;
+        }
+    }
+}
---
+++ Day17/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Concurrent;
+
+var input = File.ReadAllLines("input");
+
+var regA = long.Parse(input[0].Split(':').Last().Trim()); // operand 4
+var regB = long.Parse(input[1].Split(':').Last().Trim()); // operand 5
+var regC = long.Parse(input[2].Split(':').Last().Trim()); // operand 6
+var program = input[4].Split(':').Last().Split(',').Select(long.Parse).ToArray();
+
+var computer = new Computer(regA, regB, regC, program);
+computer.RunProgram();
+Console.WriteLine($"Part One: {string.Join(",", computer.Output)}");
+
+var partTwo = new ConcurrentBag<long>(); // Brute force, cleaning my house today...
+var buffer = 0;
+const int ChunkMax = 9999999;
+while (partTwo.IsEmpty)
+{
+    Parallel.ForEach(Enumerable.Range(0, ChunkMax), (i, j) =>
+    {
+        var testA = buffer > 0 ? buffer * ChunkMax + i : i;
+        var testCpu = new Computer(testA, regB, regC, program, true);
+        testCpu.RunProgram();
+
+        if (!testCpu.Output.SequenceEqual(program)) return;
+
+        partTwo.Add(testA);
+        Console.WriteLine($"Part Two Working Value: {testA}");
+    });
+
+    buffer++;
+    Console.WriteLine($"Buffered Times: {buffer}");
+}
+
+Console.WriteLine($"Part Two Smallest: {partTwo.Min()}");
